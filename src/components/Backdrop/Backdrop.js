61ceedcab2295377f8f88ba9879045aa047a5f16
Tony Tran <tonytranme0@hotmail.com>
2020-04-08 13:38:41 +1000

    Preparing Demo Project

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = (props) => {
+    const cssClassees = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed']
+    
+    return <div className={cssClassees.join(' ')}></div>
+};
+
+    
+
 
 export default backdrop;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import './Modal.css';
 
-const modal = (props) => (
-    <div className="Modal">
+const modal = (props) => {
+    const cssClassees = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed']
+
+    return(
+        <div className={cssClassees.join(' ')}>
         <h1>A Modal</h1>
         <button className="Button" onClick={props.closed}>Dismiss</button>
     </div>
-);
+    );
+  
+};
 
 export default modal;
