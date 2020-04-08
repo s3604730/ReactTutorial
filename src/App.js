68e11ea2aac9faa86e95469ba1da3335afe31700
Tony Tran <tonytranme0@hotmail.com>
2020-04-08 14:35:56 +1000

    Using Transition Component

--- src/App.js
+++ src/App.js
@@ -46,8 +46,19 @@ class App extends Component {
         }}/>)}
        
         </Transition>
-        {this.state.modalIsOpen ? <Modal show = {this.state.modalIsOpen} closed = {this.closeModal}/>: null}
-        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> :null }
+        <Transition 
+        mountOnEnter
+        unmountOnExit
+        
+        in = {this.state.modalIsOpen} timeout={300}>
+          {state => (
+            <Modal show = {state} 
+            closed = {this.closeModal}/>
+            
+          )}
+        </Transition>
+        {this.state.modalIsOpen
+         ? <Backdrop show /> :null }
         <button className="Button" onClick= {this.showModal} >Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './Modal.css';
 
 const modal = (props) => {
-    const cssClassees = ['Modal', props.show ? 'ModalOpen' : 'ModalClosed']
+    const cssClassees = ['Modal', 
+    props.show === 'entering'
+     ? 'ModalOpen' 
+     : props.show === 'exitiing' ? 'ModalClosed' : null
+    ];
 
     return(
         <div className={cssClassees.join(' ')}>
