1c305dc1a61d8433c7224c186e3b24df2627b8cc
Tony Tran <tonytranme0@hotmail.com>
2020-04-08 13:46:12 +1000

    css transitions

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
       <div className="App">
         <h1>React Animations</h1>
         <Modal show = {this.state.modalIsOpen} closed = {this.closeModal}/>
-        <Backdrop /> 
+        <Backdrop show={this.state.modalIsOpen} /> 
         <button className="Button" onClick= {this.showModal} >Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
